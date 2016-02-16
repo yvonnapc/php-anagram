8838ba4321d300632c2814eddfa5cb14b89987b5
Jason Wuerch and Yvonna Contreras <wuerchjason@gmail.com>
2016-02-16 09:43:33 -0800

    Add get/post functionality

--- app/app.php
+++ app/app.php
@@ -1,12 +1,21 @@
 <?php
     require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../src/Anagram.php';
 
     $app = new Silex\Application();
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
-    $app->get('/', function() {
-      return "Hello World";
+    $app->get('/', function() use($app){
+      return $app['twig']->render('form.html.twig');
+    });
+
+    $app->post('/shuffle', function() use($app){
+      $new_word = new Anagram;
+      $result = $new_word->makeAnagram($_POST['word']);
+      return $app['twig']->render('shuffle.html.twig', array('result' => $result));
+
+
     });
 
     return $app;
