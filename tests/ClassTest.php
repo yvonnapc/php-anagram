e2b3b7b7fa617258524b180a49c3d9d4817360da
Jason Wuerch and Yvonna Contreras <wuerchjason@gmail.com>
2016-02-16 08:30:55 -0800

    make template files

---
+++ tests/ClassTest.php
@@ -0,0 +1,21 @@
+<?php
+    require_once 'src/Class.php';
+
+    class ClassTest extends PHPUnit_Framework_TestCase
+    {
+      function test_Example()
+      {
+        //Arrange;
+
+        //Act;
+
+        //Assert;
+        
+      }
+
+
+
+    }
+
+
+?>
